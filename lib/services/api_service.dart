bfd283e1a72913f03c96f846a9a7dd0725d19bdb
tinaamini <tina.amini.b@gmail.com>
2025-03-08 10:51:48 +0330

    UPDATE:Changing Http To Dio

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,115 +1,122 @@
-import 'package:http/http.dart' as http;
 import 'package:shared_preferences/shared_preferences.dart';
-import 'dart:convert';
 import '../logic/models/VoiceToText/get_list_voice.dart';
 import '../logic/models/user_model/JwtToken.dart';
-
+import 'package:dio/dio.dart';
 
 class ApiService {
   static const String _baseUrl = 'https://caliber24.pythonanywhere.com';
   static const Map<String, String> _headers = {
     'accept': 'application/json',
     'Content-Type': 'application/json',
-    'X-CSRFTOKEN': '2WACHb1J9hx8KZgBhkTMktdJ9WONXYBB1uFlB5RVlz58N9TLHVRBphcHiKWA4Kxz',
+    'X-CSRFTOKEN':
+        '2WACHb1J9hx8KZgBhkTMktdJ9WONXYBB1uFlB5RVlz58N9TLHVRBphcHiKWA4Kxz',
   };
+  final Dio _dio;
 
-  Future<Map<String, String>> _getHeadersWithToken() async {
-    final prefs = await SharedPreferences.getInstance();
-    final token = prefs.getString('access_token');
-    print("Token fetched from SharedPreferences: $token"); // برای دیباگ
-    if (token != null) {
-      return {
-        ..._headers,
-        'Authorization': 'JWT $token',
-      };
-    }
-    return _headers;
-  }
+  ApiService()
+    : _dio = Dio(
+        BaseOptions(
+          baseUrl: _baseUrl,
+          headers: _headers,
+          connectTimeout: const Duration(seconds: 10),
+          receiveTimeout: const Duration(seconds: 10),
+        ),
+      );
 
   Future<Map<String, dynamic>?> createUserWithImei(String imei) async {
-    final url = Uri.parse('$_baseUrl/auth/users/');
-    final body = jsonEncode({'imei': imei});
-
     try {
-      final response = await http.post(url, headers: _headers, body: body);
-      print("Create user - API status code: ${response.statusCode}");
-      print("Create user - API response body: ${response.body}");
+      final response = await _dio.post(
+        '/auth/users/',
+        options: Options(headers: _headers),
+        data: {'imei': imei},
+      );
+      //
+      // print("Create user - API status code: ${response.statusCode}");
+      // print("Create user - API response body: ${response.data}");
       if (response.statusCode == 201) {
-        print('کاربر ثبت شد: ${response.body}');
-        return jsonDecode(response.body);
+        // print('کاربر ثبت شد: ${response.data}');
+        return response.data as Map<String, dynamic>;
       } else {
-        print('خطا در ثبت کاربر: ${response.statusCode}');
+        // print('خطا در ثبت کاربر: ${response.statusCode}');
         return null;
       }
     } catch (e) {
-      print('مشکل ارتباط در ثبت کاربر: $e');
+      // print('مشکل ارتباط در ثبت کاربر: $e');
       return null;
     }
   }
 
   Future<JwtToken?> registerIMEI(String imei) async {
-    final url = Uri.parse('$_baseUrl/auth/jwt/create/');
-    final body = jsonEncode({'imei': imei});
-
     try {
-      final response = await http.post(url, headers: _headers, body: body);
-      print("Auth - API status code: ${response.statusCode}");
-      print("Auth - API response body: ${response.body}");
+    final response = await _dio.post(
+      '/auth/jwt/create/',
+      options: Options(headers: _headers),
+      data: {'imei': imei},
+    );
+    // print("Auth - API status code: ${response.statusCode}");
+    // print("Auth - API response body: ${response.data}");
+    if (response.statusCode == 200) {
+      // print('احراز هویت شد: ${response.data}');
+      return JwtToken.fromJson(response.data);
+    } else {
+      // print('خطا در احراز هویت: ${response.statusCode}');
+      return null;
+    }
+  } catch (e) {
+  // print('مشکل ارتباط در احراز هویت: $e');
+  return null;
+  }
+}
+
+  Future<Map<String, String>> _getHeadersWithToken() async {
+    final prefs = await SharedPreferences.getInstance();
+    final token = prefs.getString('access_token');
+    print("Token fetched from SharedPreferences: $token");
+    if (token != null) {
+      return {..._headers, 'Authorization': 'JWT $token'};
+    }
+    return _headers;
+  }
+
+  Future <List<GetListVoice>?>getVoiceToText() async {
+    try{
+      final headers = await _getHeadersWithToken();
+      final response = await _dio.get(
+        "/voice-to-text/",
+        options:await Options (headers: headers)
+      );
+      // print("VTT - API status code: ${response.statusCode}");
+      // print("VTT - API response body: ${response.data}");
       if (response.statusCode == 200) {
-        print('احراز هویت شد: ${response.body}');
-        return JwtToken.fromJson(jsonDecode(response.body));
+        final List<dynamic> jsonList = response.data;
+        return jsonList.map((json) => GetListVoice.fromJson(json)).toList();
       } else {
-        print('خطا در احراز هویت: ${response.statusCode}');
+        // print('خطا در گرفتن داده‌ها: ${response.statusCode}');
         return null;
       }
     } catch (e) {
-      print('مشکل ارتباط در احراز هویت: $e');
+      // print('مشکل ارتباط در گرفتن داده‌ها: $e');
       return null;
     }
   }
   Future<bool> deleteVoice(String id) async {
-    final url = Uri.parse('$_baseUrl/voice-to-text/$id/');
-    final headers = await _getHeadersWithToken();
-    print("Headers being sent: $headers");
+    try{
+      final headers= await _getHeadersWithToken();
+      final response=await _dio.delete(
+        '/voice-to-text/$id/',
+        options: Options(headers: headers),
 
-    try {
-      final response = await http.delete(url, headers: headers);
-      print("del - API status code: ${response.statusCode}");
-      print("del - API response body: ${response.body}");
-
-      if (response.statusCode == 200 || response.statusCode == 204) {
+      );
+      if (response.statusCode ==200 || response.statusCode==204) {
         return true;
-      } else {
-        print("خطا در حذف: ${response.statusCode} - ${response.body}");
+      }else{
         return false;
       }
-    } catch (e) {
-      print('مشکل ارتباط در حذف داده‌ها: $e');
+    }catch(e){
       return false;
     }
+
   }
 
 
-
-  Future<List<GetListVoice>?> getVoiceToText() async {
-    final url = Uri.parse('$_baseUrl/voice-to-text/');
-    final headers = await _getHeadersWithToken();
-    print("Headers being sent: $headers");
-
-    try {
-      final response = await http.get(url, headers: headers);
-      print("VTT - API status code: ${response.statusCode}");
-      print("VTT - API response body: ${response.body}");
-      if (response.statusCode == 200) {
-        final List<dynamic> jsonList = jsonDecode(response.body);
-        return jsonList.map((json) => GetListVoice.fromJson(json)).toList();
-      } else {
-        print('خطا در گرفتن داده‌ها: ${response.statusCode}');
-        return null;
-      }
-    } catch (e) {
-      print('مشکل ارتباط در گرفتن داده‌ها: $e');
-      return null;
-    }
-  }
 }
